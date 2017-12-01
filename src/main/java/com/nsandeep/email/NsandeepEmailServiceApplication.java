da43f378e947fcf9dd1464666434c97428935450
nsandeep <nsandeepdotcom@gmail.com>
2017-11-30 18:30:05 -0600

    nsandeep-email-service intial commit

---
+++ src/main/java/com/nsandeep/email/NsandeepEmailServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nsandeep.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsandeepEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsandeepEmailServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nsandeep/email/NsandeepEmailServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nsandeep.email;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NsandeepEmailServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
